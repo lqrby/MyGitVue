888f2dbf3a99688dfed4dfa034694e1a98585a53
lqrby <748862180@qq.com>
2020-12-04 11:34:07 +0800

    提交同创源码项目

---
+++ TongChuangYuanMa/yunqianbao/common/userMobile.py
@@ -0,0 +1,14 @@
+
+def user_mobile():
+    mobile = {
+        "start":15000001201, #7766没有注册成功
+        "end":15000001204
+    }
+    # mobile = {
+    #     "start":18810798467, #7766没有注册成功
+    #     "end":18810798468
+    # }
+    return mobile
+
+    
+    
